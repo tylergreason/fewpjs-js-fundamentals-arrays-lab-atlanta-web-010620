b34618271572e73a0666ef3689ac74b727b4512c
tylergreason <tyler.greason@protonmail.com>
2020-02-19 13:04:32 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your solution here!
+// const array = ["Milo", "Otis", "Garfield"]; 
+
+const append = ["Milo", "Otis", "Garfield","Odie"]; 
+
+const prepend = ["Odie", "Milo", "Otis", "Garfield"];
+
+const removeLast = ["Milo", "Otis"]; 
+
+const removeFirst = ["Otis", "Garfield"];
